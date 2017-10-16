b58c1b2c16de88a5c7baf2ce9a3392dfdba42ad5
tomvdv59 <t.vandevelde@yahoo.fr>
2017-10-16 22:18:03 +0100

    Add controllers

--- app/controllers/HelloController.scala
+++ app/controllers/HelloController.scala
@@ -5,13 +5,20 @@ import javax.inject.{Inject, Singleton}
 import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, ControllerComponents}
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 @Singleton
 class HelloController  @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def hello() = Action.async { implicit request =>
-    Future.successful(Ok("ssss"))
+  def world() = Action.async { implicit request =>
+    Future.successful(Ok(Json.obj("message" -> "Hello World")))
+  }
+
+  def application() = Action.async { implicit request =>
+    Future.successful(Ok(Json.obj("message" -> "Hello Application")))
+  }
+
+  def user() = Action.async { implicit request =>
+    Future.successful(Ok(Json.obj("message" -> "Hello User")))
   }
 }
